b87da62db01ef92f0019d433759053344f947863
Ryan Frishkorn <ryan@bluerift.io>
2022-12-06 23:47:25 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,99 @@
+// use std::collections::HashMap;
+use reqwest::header::USER_AGENT;
+// use serde::{Deserialize};
+use tokio;
+
+struct Forecast {
+    properties: Properties,
+    // periods: Vec<Period>,
+}
+
+#[allow(non_snake_case)]
+struct Properties {
+    updated: String,
+    forecastGenerator: String,
+    generatedAt: String,
+    updateTime: String,
+    validTimes: String,
+    periods: Vec<Period>,
+}
+
+#[allow(non_snake_case)]
+struct Period {
+    number: u8,
+    name: String,
+    startTime: String,
+    endTime: String,
+    isDaytime: bool,
+    temperature: u8,
+    temperatureTrend: String,
+    windSpeed: u8,
+    windDirection: String,
+    shortForecast: String,
+    detailedForecast: String,
+}
+
+#[tokio::main]
+async fn main() {
+    // println!("Weather!");
+    // Locate grid data by lat/lon
+    // let url = "https://api.weather.gov/points/36.1744,-115.2721";
+
+    // Specify grid point - this gives raw numerical data
+    // let url = "https://api.weather.gov/gridpoints/VEF/117,98";
+
+    // Forecast
+    let url = "https://api.weather.gov/gridpoints/VEF/117,98/forecast";
+
+    let client = reqwest::Client::new();
+    let response = match client.get(url)
+        .header(USER_AGENT, "rust-implementation/console")
+        .send()
+        .await {
+        Ok(r) => r,
+        Err(e) => panic!("error waiting for response: {}", e),
+    };
+
+    // println!("response: {:?}", response);
+    let text = match response.text().await {
+        Ok(t) => t,
+        Err(e) => panic!("error getting body: {}", e),
+    };
+
+    println!("{}", text);
+}
+
+// NWS Request:
+//
+// 36.17446 N, 115.27203 W
+// https://api.weather.gov/points/36.1744,-115.2721
+
+// Response Data:
+//
+// "properties": {
+//   "updated": "2022-12-06T20:12:59+00:00",
+//   "units": "us",
+//   "forecastGenerator": "BaselineForecastGenerator",
+//   "generatedAt": "2022-12-06T22:46:16+00:00",
+//   "updateTime": "2022-12-06T20:12:59+00:00",
+//   "validTimes": "2022-12-06T14:00:00+00:00/P8DT6H",
+//   "elevation": {
+//     "unitCode": "wmoUnit:m",
+//     "value": 779.9832
+//   },
+//   "periods": [
+//     {
+//       "number": 1,
+//       "name": "This Afternoon",
+//       "startTime": "2022-12-06T14:00:00-08:00",
+//       "endTime": "2022-12-06T18:00:00-08:00",
+//       "isDaytime": true,
+//       "temperature": 56,
+//       "temperatureUnit": "F",
+//       "temperatureTrend": "falling",
+//       "windSpeed": "3 mph",
+//       "windDirection": "ESE",
+//       "icon": "https://api.weather.gov/icons/land/day/bkn?size=medium",
+//       "shortForecast": "Partly Sunny",
+//       "detailedForecast": "Partly sunny. High near 56, with temperatures falling to around 53 in the afternoon. East southeast wind around 3 mph."
+//     },
